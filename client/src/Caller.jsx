1b628e1e6befd93f07f0052e833ac5d23f0d1f3b
mark-marshall <mark.marshallgp@gmail.com>
2022-08-01 13:25:09 +0100

    First commit

--- client/src/Caller.jsx
+++ client/src/Caller.jsx
@@ -1,51 +1,87 @@
 import React, { useState, useEffect } from 'react';
 import { Device } from 'twilio-client';
 import axios from 'axios';
+import './Caller.css';
 
 const Caller = () => {
   const [device, setDevice] = useState(null);
-  const [status, setStatus] = useState(false);
-  const [token, setToken] = useState(null);
-  const [perm, setPerm] = useState(false);
-  const [num, setNum] = useState('');
+  const [incoming, setIncoming] = useState(null);
 
   const makeCall = () => {
-    device.connect({
-      phoneNumber: num,
-    });
+    if (device) {
+      device.connect({
+        // Number to call
+        recipientNumber: '+447557955350',
+      });
+    }
+  };
+
+  const acceptCall = (response) => {
+    if (response) {
+      incoming.accept();
+    } else {
+      incoming.reject();
+    }
   };
 
   useEffect(() => {
     const getToken = async () => {
-      const res = await axios.post('https://mmarshall.eu.ngrok.io/auth');
-      let newDevice = new Device(res.data.token, {
+      // Request to tken server
+      const res = await axios.post('https://mmarshall.ngrok.io/token', {
+        // This is the ID of the client device
+        identity: 'mm',
+      });
+
+      let newDevice = new Device(res.data, {
         codecPreferences: ['opus', 'pcmu'],
         fakeLocalDTMF: true,
         enableRingingState: true,
       });
+
       newDevice.on('ready', (device) => {
         console.log('Device ready!');
-        setStatus(true);
       });
+      newDevice.on('disconnect', (device) => {
+        console.log('disconnected');
+        setIncoming(null);
+      });
+
+      newDevice.on('cancel', (device) => {
+        console.log('canceled');
+      });
+
       newDevice.on('error', (err) => {
         console.log(err);
       });
+
+      newDevice.on('incoming', (connection) => {
+        console.log('incoming');
+        setIncoming(connection);
+      });
+
+      newDevice.on('reject', () => {
+        console.log('reject');
+      });
+
       setDevice(newDevice);
     };
-    if (perm) {
-      getToken();
-    }
-  }, [perm]);
+
+    getToken();
+  }, []);
 
   return (
     <>
-      <button onClick={() => setPerm(true)}>Get Started</button>
-      <input
-        value={num}
-        onChange={(e) => setNum(e.target.value)}
-        placeholder="number"
-      />
-      <button onClick={() => makeCall()}>Make Call</button>
+      <img src="https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3667&q=80" />
+      {incoming ? (
+        <div className="button-cluster inbound-controls">
+          <button onClick={() => acceptCall(true)}>Accept</button>
+          <button onClick={() => acceptCall(false)}>Reject</button>
+        </div>
+      ) : (
+        <div className="button-cluster">
+          <button onClick={() => makeCall()}>Call Estate Agent</button>
+        </div>
+      )}
     </>
   );
 };
